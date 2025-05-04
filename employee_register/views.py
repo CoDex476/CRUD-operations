5a487bbed0f69a6e7dd44fba9af037a7cee4a54a
coDex99 <abednegodunyah@gmail.com>
2025-05-04 01:41:57 +0100

    Set up employee registration with forms, views, and templates; updated settings and URL routing.

--- employee_register/views.py
+++ employee_register/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import EmployeeForm
 
-# Create your views here.
+
+def employee_form(request):
+    form = EmployeeForm()
+    context = {"form": form}
+    return render(request, "employee_register/employee_form.html", context)
+
+
+def employee_list(request):
+    return render(request, "employee_register/employee_list.html")
+
+
+def employee_delete(request):
+    return HttpResponse("<h1>delete page</h1>")
